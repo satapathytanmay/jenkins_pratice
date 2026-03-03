6ab43702975b2dcf93bca1ae474107397eb69f7e
satapathytanmay <satapathyt8@Gmail.com>
2026-03-03 09:59:28 +0530

    Fix main method parameter syntax

--- app/src/main/java/jar/App.java
+++ app/src/main/java/jar/App.java
@@ -6,7 +6,7 @@ package jar;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args 1 )
     {
         System.out.println( "Hello World!" );
     }
